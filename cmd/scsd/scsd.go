6f84d1f042cc1b5e2a96aa5500f1861567d04ae2
Joshua Williams <102987390+jwlv@users.noreply.github.com>
2025-05-22 14:19:43 -0700

    CASMHMS-6398: Is logging working??

--- cmd/scsd/scsd.go
+++ cmd/scsd/scsd.go
@@ -297,6 +297,11 @@ func main() {
 	}
 
 	//Fetch the service instance name
+logger.Trace("JW_DEBUG: Trace")
+logger.Debug("JW_DEBUG: Debug")
+logger.Info("JW_DEBUG: Info")
+logger.Warn("JW_DEBUG: Warn")
+logger.Error("JW_DEBUG: Error")
 
 	serviceName, err = base.GetServiceInstanceName()
 	if err != nil {
